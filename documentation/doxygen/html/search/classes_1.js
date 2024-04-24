d4b5e1ad5219cdebe051ca854370509461ead971
Julia Clement <32722175+juliaclement@users.noreply.github.com>
2024-04-25 08:02:21 +1200

    Refresh Doxygen

--- documentation/doxygen/html/search/classes_1.js
+++ documentation/doxygen/html/search/classes_1.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['countedpointer_0',['CountedPointer',['../classjclib_1_1CountedPointer.html',1,'jclib']]],
-  ['countedpointer_3c_20class_20weakpointerbase_20_3e_1',['CountedPointer&lt; class WeakPointerBase &gt;',['../classjclib_1_1CountedPointer.html',1,'jclib']]],
-  ['countedpointer_3c_20jclib_3a_3ayieldingiteratorhelper_3c_20t_20_3e_20_3e_2',['CountedPointer&lt; jclib::yieldingIteratorHelper&lt; T &gt; &gt;',['../classjclib_1_1CountedPointer.html',1,'jclib']]],
-  ['countedpointer_3c_20private_3a_3ajstringbase_20_3e_3',['CountedPointer&lt; Private::jStringBase &gt;',['../classjclib_1_1CountedPointer.html',1,'jclib']]],
-  ['countedpointersafe_4',['CountedPointerSafe',['../classjclib_1_1CountedPointerSafe.html',1,'jclib']]],
-  ['countedpointertarget_5',['CountedPointerTarget',['../classjclib_1_1CountedPointerTarget.html',1,'jclib']]],
-  ['countedpointertestclass_6',['CountedPointerTestClass',['../classCountedPointerTestClass.html',1,'']]],
-  ['countedpointertestdata_7',['CountedPointerTestData',['../classCountedPointerTestData.html',1,'']]]
+  ['config_0',['config',['../classjclib_1_1config.html',1,'jclib']]],
+  ['countedpointer_1',['CountedPointer',['../classjclib_1_1CountedPointer.html',1,'jclib']]],
+  ['countedpointer_3c_20class_20weakpointerbase_20_3e_2',['CountedPointer&lt; class WeakPointerBase &gt;',['../classjclib_1_1CountedPointer.html',1,'jclib']]],
+  ['countedpointer_3c_20jclib_3a_3ayieldingiteratorhelper_3c_20t_20_3e_20_3e_3',['CountedPointer&lt; jclib::yieldingIteratorHelper&lt; T &gt; &gt;',['../classjclib_1_1CountedPointer.html',1,'jclib']]],
+  ['countedpointer_3c_20private_3a_3ajstringbase_20_3e_4',['CountedPointer&lt; Private::jStringBase &gt;',['../classjclib_1_1CountedPointer.html',1,'jclib']]],
+  ['countedpointersafe_5',['CountedPointerSafe',['../classjclib_1_1CountedPointerSafe.html',1,'jclib']]],
+  ['countedpointertarget_6',['CountedPointerTarget',['../classjclib_1_1CountedPointerTarget.html',1,'jclib']]],
+  ['countedpointertestclass_7',['CountedPointerTestClass',['../classCountedPointerTestClass.html',1,'']]],
+  ['countedpointertestdata_8',['CountedPointerTestData',['../classCountedPointerTestData.html',1,'']]]
 ];
